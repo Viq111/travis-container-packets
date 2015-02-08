c00bb4e9a9afc275f56cf2098365dd97a3f76e69
Vianney Tran <viq.dev@gmail.com>
2015-02-08 22:54:19 +0100

    Build cmake 2.8.12

---
+++ build_cmake.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -e
+# Get sources
+echo "Getting sources..."
+wget http://www.cmake.org/files/v2.8/cmake-2.8.12.tar.gz
+echo "Untarring..."
+tar -xzf cmake-*.tar.gz
+rm cmake-*.tar.gz
+cd cmake-*
+
+# Build library
+echo "Building..."
+./bootstrap
+make
+# Rearrange folders
+make package
+mkdir build
+mv cmake-*.tar.gz build
+cd build
+tar -xzf cmake-*.tar.gz
+rm cmake-*.tar.gz
+mv cmake-* cmake
+# Tar library
+echo "Build done, tarring..."
+tar -jc --file=cmake.tar.bz2 cmake
+echo "Uploading..."
+echo "########################################################################"
+echo "Build URL:"
+curl --upload-file ./cmake.tar.bz2 https://transfer.sh/cmake.tar.bz2
+echo "########################################################################"
+# Clean dir
+cd ../..
+rm -r cmake-*
