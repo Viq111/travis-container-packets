a0bed6b6f6230e09317e2ed52555c3da99cbf1f3
Vianney Tran <viq.dev@gmail.com>
2015-02-09 00:35:42 +0100

    Trying to build gcc 4.8

---
+++ build_gcc.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+wget http://ftp.gnu.org/gnu/gcc/gcc-4.8.2/gcc-4.8.2.tar.bz2
+tar -xjf gcc-4.8.2.tar.bz2
+cd gcc-4.8.2/
+mkdir build
+cd build
+../configure --disable-checking --enable-languages=c,c++ --enable-multiarch --enable-shared --enable-threads=posix --program-suffix=4.8 --without-included-gettext --with-system-zlib --prefix=./gcc
+make -j 4
+make install
+# Tar library
+echo "Build done, tarring..."
+tar -jc --file=gcc.tar.bz2 gcc
+echo "Uploading..."
+echo "########################################################################"
+echo "Build URL:"
+curl --upload-file ./gcc.tar.bz2 https://transfer.sh/gcc.tar.bz2
+echo "########################################################################"
