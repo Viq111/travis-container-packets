cfb1e152a482c8dab2695c2e3dfd09dc3204ad86
Vianney Tran <viq.dev@gmail.com>
2015-02-09 00:51:28 +0100

    Cosmetic changes + removed multi-arch flag

--- build_gcc.sh
+++ build_gcc.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 set -e
+# Get sources
+echo "Getting sources..."
 wget http://ftp.gnu.org/gnu/gcc/gcc-4.8.2/gcc-4.8.2.tar.bz2
+echo "Untarring..."
 tar -xjf gcc-4.8.2.tar.bz2
+rm gcc-4.8.2.tar.bz2
 cd gcc-4.8.2/
+
+# Build library
+echo "Downloading prerequisites..."
 ./contrib/download_prerequisites
 mkdir build
 cd build
-../configure --enable-languages=c,c++ --enable-multiarch --enable-shared --enable-threads=posix --program-suffix=4.8 --without-included-gettext --with-system-zlib --prefix=$(pwd)/gcc
+echo "Configuring..."
+../configure --enable-languages=c,c++ --enable-shared --enable-threads=posix --program-suffix=4.8 --without-included-gettext --with-system-zlib --prefix=$(pwd)/gcc
+echo "Building..."
 make -j 4
 make install
 # Tar library
