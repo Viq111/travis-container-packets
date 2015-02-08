22f9e39018147837560a5d07f4c1490d18991359
Vianney Tran <viq.dev@gmail.com>
2015-02-08 22:39:41 +0100

    Commented unused lines

--- build_base.sh
+++ build_base.sh
@@ -13,9 +13,9 @@ echo "Building..."
 
 # Tar library
 echo "Build done, tarring..."
-tar -jc --file=compiled.tar.bz2 build
+#tar -jc --file=compiled.tar.bz2 build
 echo "Uploading..."
 echo "########################################################################"
 echo "Build URL:"
-curl --upload-file ./compiled.tar.bz2 https://transfer.sh/compiled.tar.bz2
+#curl --upload-file ./compiled.tar.bz2 https://transfer.sh/compiled.tar.bz2
 echo "########################################################################"
