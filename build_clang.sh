9b08217f31579f5f61a43f14e5f8e781fda0be22
Vianney Tran <viq.dev@gmail.com>
2015-02-09 18:49:24 +0100

    trying to build clang from sources

--- build_clang.sh
+++ build_clang.sh
@@ -1,16 +1,31 @@
 #!/bin/sh
 set -e
-cd install_clang
-mkdir -p build/clang
-
-# Build library
-echo "Building..."
-./install-clang -j 4 -m $(pwd)/build/clang
-
+mkdir clang
+cd clang
+echo "Getting LLVM..."
+svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
+echo "Getting Clang..."
+cd llvm/tools
+svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
+cd ../..
+echo "Getting extras..."
+cd llvm/tools/clang/tools
+svn co http://llvm.org/svn/llvm-project/clang-tools-extra/trunk extra
+cd ../../../..
+echo "Getting RT..."
+cd llvm/projects
+svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
+cd ../..
+echo "Configuring..."
+mkdir build
+cd build
+mkdir binaries
+../llvm/configure --prefix=$(pwd)/binaries
+make -j4
 # Tar library
 echo "Build done, tarring..."
 cd build
-tar -jc --file=clang.tar.bz2 clang
+tar -jc --file=clang.tar.bz2 binaries
 echo "Uploading..."
 echo "########################################################################"
 echo "Build URL:"
