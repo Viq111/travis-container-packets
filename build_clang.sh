18fc4fd4249799148b27e2048d1f1bdc512f9cb7
Vianney Tran <viq.dev@gmail.com>
2015-02-09 11:59:31 +0100

    Build clang

---
+++ build_clang.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+cd install_clang
+mkdir -p build/clang
+
+# Build library
+echo "Building..."
+./install-clang -j 4 $(pwd)/build/clang
+
+# Tar library
+echo "Build done, tarring..."
+cd build
+tar -jc --file=clang.tar.bz2 clang
+echo "Uploading..."
+echo "########################################################################"
+echo "Build URL:"
+curl --upload-file ./clang.tar.bz2 https://transfer.sh/clang.tar.bz2
+echo "########################################################################"
+cd ..
+rm -rf build
