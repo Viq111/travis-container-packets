527b6fe3e9d3fa09db8899ad1b47bc54684a8d79
Vianney Tran <viq.dev@gmail.com>
2015-02-15 11:43:50 +0100

    Building GCC 4.9.2

--- build_gcc.sh
+++ build_gcc.sh
@@ -2,11 +2,12 @@
 set -e
 # Get sources
 echo "Getting sources..."
-wget http://ftp.gnu.org/gnu/gcc/gcc-4.8.2/gcc-4.8.2.tar.bz2
+export GCC_BUILD_VERSION=4.9.2
+wget http://ftp.gnu.org/gnu/gcc/gcc-$GCC_BUILD_VERSION/gcc-$GCC_BUILD_VERSION.tar.bz2
 echo "Untarring..."
-tar -xjf gcc-4.8.2.tar.bz2
-rm gcc-4.8.2.tar.bz2
-cd gcc-4.8.2/
+tar -xjf gcc-$GCC_BUILD_VERSION.tar.bz2
+rm gcc-$GCC_BUILD_VERSION.tar.bz2
+cd gcc-$GCC_BUILD_VERSION/
 
 # Build library
 echo "Downloading prerequisites..."
@@ -17,15 +18,15 @@ echo "Configuring..."
 export LIBRARY_PATH=/usr/lib/$(gcc -print-multiarch)
 export C_INCLUDE_PATH=/usr/include/$(gcc -print-multiarch)
 export CPLUS_INCLUDE_PATH=/usr/include/$(gcc -print-multiarch)
-mkdir -p gcc-4.8.2/gcc
-../configure --enable-languages=c,c++ --disable-multilib --enable-shared --enable-threads=posix --prefix=$(pwd)/gcc-4.8.2/gcc
+mkdir -p gcc-$GCC_BUILD_VERSION/gcc
+../configure --enable-languages=c,c++ --disable-multilib --enable-shared --enable-threads=posix --prefix=$(pwd)/gcc-$GCC_BUILD_VERSION/gcc
 echo "Building..."
 make -j 4 | awk '{printf "."}'
 echo ""
 make install
 # Tar library
 echo "Build done, tarring..."
-cd gcc-4.8.2
+cd gcc-$GCC_BUILD_VERSION/
 tar -jc --file=gcc.tar.bz2 gcc
 echo "Uploading..."
 echo "########################################################################"
