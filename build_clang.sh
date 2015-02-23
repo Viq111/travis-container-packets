2c04930d35e1d501b7d9e50c97dc383a0bfcb1ca
Vianney Tran <viq.dev@gmail.com>
2015-02-23 10:26:26 +0100

    Download headers from the precompiled version

--- build_clang.sh
+++ build_clang.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 export LLVM_VERSION=RELEASE_351
+export LLVM_PRECOMPILED="http://llvm.org/releases/3.5.1/clang+llvm-3.5.1-x86_64-linux-gnu.tar.xz"
 mkdir clang_build/
 cd clang_build/
 echo "> Compiling Clang $LLVM_VERSION ..."
@@ -25,9 +26,16 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX=$(readlink -f $(pwd)/../clang) -DLLVM_TARGETS_TO_BUILD="X86" ../llvm
 make -j4
 make install
+# Let's cheat, download headers from precompiled
+echo "Build done, downloading headers..."
+cd ..
+wget $LLVM_PRECOMPILED
+tar -xf clang+llvm*.tar.xz
+cd clang+llvm*/include/
+mv c++/ ../../clang/include/c++/
+cd ../..
 # Tar library
 echo "Build done, tarring..."
-cd ..
 tar -jc --file=clang.tar.bz2 clang/
 echo "Uploading..."
 echo "########################################################################"
